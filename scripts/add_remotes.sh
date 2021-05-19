d4b6df1b205459de4b69363225424b1c4d186a34
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-20 00:12:24 +0300

    cosmetics

--- scripts/add_remotes.sh
+++ scripts/add_remotes.sh
@@ -4,6 +4,6 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git remote add github    "git@github.com:andrew-aladev/argtable3.git"    || true
-git remote add bitbucket "git@bitbucket.org:andrew-aladev/argtable3.git" || true
-git remote add gitlab    "git@gitlab.com:andrew-aladev/argtable3.git"    || true
+git remote add github    "git@github.com:andrew-aladev/argtable3.git"    || :
+git remote add bitbucket "git@bitbucket.org:andrew-aladev/argtable3.git" || :
+git remote add gitlab    "git@gitlab.com:andrew-aladev/argtable3.git"    || :
