67b00f1cb442c762ea8331d699f3a7e5588da2e6
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-01-07 13:16:22 +0300

    scripts cosmetics

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 cd "$DIR"
 
-git remote | xargs -n1 git push --all
-git remote | xargs -n1 git push --tags
+git remote | xargs -n1 git push --all || :
+git remote | xargs -n1 git push --tags || :
